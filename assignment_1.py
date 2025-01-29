454856f22bb013405129dd609433c234b21ec3c7
zcong0202 <zheng.cong@kaust.edu.sa>
2025-01-29 16:58:41 +0300

    Added code to visualize Jeddah's topography

---
+++ assignment_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdb
+import xarray as xr
+
+
+dset = xr.open_dataset(r'C:\Users\congz\OneDrive - KAUST\Desktop(DELL)\ERSE316 ASSIGNMENT\N21E039.SRTMGL1_NC.nc')
+pdb.set_trace()
+
